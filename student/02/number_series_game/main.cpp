a395936b09e9c620b799b2882029fa7582bc9739
Elsa Huovila <elsa.huovila@tuni.fi>
2021-09-06 21:07:10 +0300

    Palautus

--- student/02/number_series_game/main.cpp
+++ student/02/number_series_game/main.cpp
@@ -3,5 +3,19 @@
 
 int main()
 {
+    int numbers_amnt = 0;
     std::cout << "How many numbers would you like to have? ";
+    std::cin >> numbers_amnt;
+
+    for (int i = 1; i < numbers_amnt + 1; ++i) {
+        if (i % 3 == 0 && i % 7 == 0) {
+            std::cout << "zip boing" << std::endl;
+        } else if (i % 3 == 0) {
+            std::cout << "zip" << std::endl;
+        } else if (i % 7 == 0) {
+            std::cout << "boing" << std::endl;
+        } else {
+            std::cout << i << std::endl;
+        }
+    }
 }

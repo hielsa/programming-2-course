defddbf3966609eb108d4ffff1075dbde0278e30
Elsa Huovila <elsa.huovila@tuni.fi>
2021-09-06 20:46:19 +0300

    Palautus

--- student/02/temperature/main.cpp
+++ student/02/temperature/main.cpp
@@ -4,9 +4,16 @@ using namespace std;
 
 int main()
 {
+    int temp = 0;
+    double C = 0;
+    double F = 0;
     cout << "Enter a temperature: ";
+    cin >> temp;
+    F = (temp*1.8)+32;
+    C = (temp-32) / 1.8;
 
-    // Write your code here
+    cout << temp << " degrees Celsius is " << F << " degrees Fahrenheit" << endl;
+    cout << temp << " degrees Celsius is " << C << " degrees Celsius" << endl;
 
     return 0;
 }

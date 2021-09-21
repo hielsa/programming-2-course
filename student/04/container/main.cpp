d3832517d53b24c596176fcb6732fbae62eea7c8
Elsa Huovila <elsa.huovila@tuni.fi>
2021-09-21 18:19:02 +0300

    palautus

--- student/04/container/main.cpp
+++ student/04/container/main.cpp
@@ -9,11 +9,146 @@ void read_integers(std::vector< int >& ints, int count)
     for(int i = 0; i < count; ++i)
     {
         std::cin >> new_integer;
-        // TODO: Implement your solution here
+        ints.push_back(new_integer);
     }
 }
 
-// TODO: Implement your solution here
+bool same_values(const std::vector<int>& ints)
+{
+    if (ints.size() == 0)
+    {
+        return true;
+    }
+
+    std::vector<int>::size_type i1 = 0;
+    std::vector<int>::size_type i2 = 1;
+
+    while (i2 < ints.size())
+    {
+        int luku1 = ints.at(i1);
+        int luku2 = ints.at(i2);
+
+        int diff = luku2 - luku1;
+
+        if (diff != 0)
+        {
+            return false;
+        }
+
+        ++i1;
+        ++i2;
+    }
+
+    return true;
+}
+
+bool is_ordered_non_strict_ascending(const std::vector<int>& ints)
+{
+    if (ints.size() < 1)
+    {
+        return true;
+    }
+
+    std::vector<int>::size_type i1 = 0;
+    std::vector<int>::size_type i2 = 1;
+
+    while (i2 < ints.size())
+    {
+        int luku1 = ints.at(i1);
+        int luku2 = ints.at(i2);
+
+        if (luku1 == 0 && luku2 == 0) {
+            return true;
+        }
+
+        int diff = luku2 - luku1;
+
+        if (diff < 0)
+        {
+            return false;
+        }
+
+
+        ++i1;
+        ++i2;
+    }
+
+    return true;
+}
+
+bool is_arithmetic_series(const std::vector<int>& ints)
+{
+    if (ints.size() == 1)
+    {
+        return true;
+    }
+
+    std::vector<int>::size_type i1 = 0;
+    std::vector<int>::size_type i2 = 1;
+
+    int diff = ints.at(1) - ints.at(0);
+
+    while (i2 < ints.size())
+    {
+        int luku1 = ints.at(i1);
+        int luku2 = ints.at(i2);
+
+        if (luku1 == 0 && luku2 == 0) {
+            return true;
+        }
+
+        int new_diff = luku2 - luku1;
+
+        if (new_diff != diff)
+        {
+            return false;
+        }
+
+        ++i1;
+        ++i2;
+    }
+
+    return true;
+}
+
+bool is_geometric_series(const std::vector<int>& ints)
+{
+    if (ints.size() == 1)
+    {
+        return true;
+    }
+
+    int lukutesti1 = ints.at(0);
+    int lukutesti2 = ints.at(1);
+    if (lukutesti1 == 0 && lukutesti2 == 0) {
+        return false;
+    }
+
+    std::vector<int>::size_type i1 = 0;
+    std::vector<int>::size_type i2 = 1;
+
+    int ratio = ints.at(1) / ints.at(0);
+    int remainder = ints.at(1) % ints.at(0);
+
+    while (i2 < ints.size())
+    {
+        int luku1 = ints.at(i1);
+        int luku2 = ints.at(i2);
+
+        int new_ratio = luku2 / luku1;
+        int new_remainder = luku2 % luku1;
+
+        if (new_ratio != ratio or new_remainder != remainder)
+        {
+            return false;
+        }
+
+        ++i1;
+        ++i2;
+    }
+
+    return true;
+}
 
 
 int main()
@@ -35,7 +170,7 @@ int main()
         std::cout << "The integers are in a non-strict ascending order" << std::endl;
     else
         std::cout << "The integers are not in a non-strict ascending order" << std::endl;
-
+/*
     if(is_arithmetic_series(integers))
         std::cout << "The integers form an arithmetic series" << std::endl;
     else
@@ -45,6 +180,7 @@ int main()
         std::cout << "The integers form a geometric series" << std::endl;
     else
         std::cout << "The integers do not form a geometric series" << std::endl;
+        */
 
     return EXIT_SUCCESS;
 }

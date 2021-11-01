a2c68c6b1d10b27186b3b5d415d4ddc7ac2b87f2
Elsa Huovila <elsa.huovila@tuni.fi>
2021-11-01 17:07:50 +0200

    korjaus funktioihin

--- student/08/arrays/array_operations.cpp
+++ student/08/arrays/array_operations.cpp
@@ -1,1 +1,47 @@
 #include "array_operations.hh"
+
+#include "algorithm"
+
+int greatest_v1(int* itemptr, int size)
+{
+    int greatest = *itemptr;
+    for (int i=0; i < size; ++i)
+    {
+        int current = *(itemptr + i);
+        if (current > greatest)
+        {
+            greatest = current;
+        }
+    }
+    return greatest;
+}
+
+int greatest_v2(int *itemptr, int *endptr)
+{
+    int greatest = *itemptr;
+    for (; itemptr != endptr; ++ itemptr)
+    {
+        int current = *itemptr;
+        greatest = std::max(greatest, current);
+    }
+    return greatest;
+}
+
+void copy(int *itemptr, int *endptr, int *targetptr)
+{
+    for (; itemptr != endptr; ++itemptr, ++targetptr)
+    {
+        *targetptr = *itemptr;
+    }
+
+}
+
+void reverse(int *leftptr, int *rightptr)
+{
+    for (--rightptr; leftptr < rightptr; ++leftptr, --rightptr)
+    {
+        int tmp = *leftptr;
+        *leftptr = *rightptr;
+        *rightptr = tmp;
+    }
+}

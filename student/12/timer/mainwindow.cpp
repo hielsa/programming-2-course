9bcb1e33402b2c2d2ca45e25032b800549a6ae44
Elsa Huovila <elsa.huovila@tuni.fi>
2021-11-29 23:32:55 +0200

    Timer

---
+++ student/12/timer/mainwindow.cpp
@@ -0,0 +1,63 @@
+#include "mainwindow.hh"
+#include "ui_mainwindow.h"
+
+#include <iostream>
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+    connect(ui->startButton, &QPushButton::clicked, this, &MainWindow::onStartButtonClicked);
+    connect(ui->stopButton, &QPushButton::clicked, this, &MainWindow::onStopButtonClicked);
+    connect(ui->resetButton, &QPushButton::clicked, this, &MainWindow::onResetButtonClicked);
+    connect(ui->closeButton, &QPushButton::clicked, this, &MainWindow::onCloseButtonClicked);
+
+    timer = new QTimer(this);
+    connect(timer, &QTimer::timeout, this, &MainWindow::onSecTimer);
+
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::onStartButtonClicked()
+{
+    timer->start(1000);
+}
+
+void MainWindow::onStopButtonClicked()
+{
+    timer->stop();
+}
+
+void MainWindow::onResetButtonClicked()
+{
+    ui->lcdNumberMin->display(0);
+    ui->lcdNumberSec->display(0);
+}
+
+void MainWindow::onCloseButtonClicked()
+{
+    close();
+}
+
+void MainWindow::onSecTimer()
+{
+    int seconds = ui->lcdNumberSec->intValue();
+    int minutes = ui->lcdNumberMin->intValue();
+
+    ++seconds;
+    if (seconds == 60)
+    {
+        seconds = 0;
+        ++minutes;
+    }
+
+    ui->lcdNumberSec->display(seconds);
+    ui->lcdNumberMin->display(minutes);
+
+}
